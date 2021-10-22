9e44ea426a8e704ed7b48fe03f1b14f5dd3fbe82
Sathish Kumar <84026483+sathishwebdev@users.noreply.github.com>
2021-10-22 17:02:20 +0530

    update trailer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // import logo from './logo.svg';
 import React, {useState} from 'react'
 import './App.css';
-import movies from './movies.json'
+import movies from './movies.json' 
+var temp = ['1']
 function App() {
 const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
       
     
-   const getById = (id) =>setMovie(movies.filter(data=>data.id === id))
+   const getById = (id) =>{setMovie(movies.filter(data=>data.id === id))
+   temp[1] = temp[0] 
+    temp[0] = id
+    console.log(temp)
+      document.getElementById(temp[1]).style.border = "none";
+      document.getElementById(temp[1]).style.backgroundColor= "transparent"
+      document.getElementById(temp[1]).style.boxShadow="none"
+    document.getElementById(id).style.border = "1px grey solid"
+    document.getElementById(id).style.borderRadius="5%"
+    document.getElementById(id).style.backgroundColor="gray"
+    document.getElementById(id).style.boxShadow="black 1px 2px 15px"
+  
+  }
    
  
   return (
@@ -15,9 +28,9 @@ const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
      {
        movies.map(({id, name, poster, category, watchOn, summary, releaseDate, genre})=>(
         
-        <div key={name} className="movieList">
+        <div key={name} id={id} className="movieList">
         <div className="posterCon" ><button 
-         style={{padding:"0", border: 'none', width:'100%'}}
+         style={{padding:"0", border: 'none'}}
           
          onClick = {()=>getById(id)}
         >  <img src={poster} className="poster" alt={name} title={name} /> </button>  </div><p className="name">{name}</p>
@@ -27,14 +40,21 @@ const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
        ))
      }
     </div></div>
-  {  movie.map(({id,name, poster, category, watchOn, summary, releaseDate, genre})=>(
-    <div key={id} className="movieCon"><a
+  {  movie.map(({id,name, poster,trailer, category, watchOn, summary, releaseDate, genre})=>(
+    <div key={id}>
+    
+    <h1 className="name">{name}</h1>
+    
+     <div  className="movieCon ">
+    
+ 
+       <div className="content "> 
+      <a
           className="App-link"
           href={watchOn.link}
           target="_blank"
           rel="noopener noreferrer"
-        >  <img src={poster} className="contentImg" alt={name} title={name} /> </a> 
-       <div className="content"> <h2 className="name">{name}</h2>
+        >  <img src={poster} className="contentImg" alt={name} title={name} /> </a> <p> <a href={`https:youtu.be/${trailer.split("/")[trailer.split("/").length-1]}`} className="App-link"> Watch Trailer  </a></p>
         <p> category :  {category} </p>
         <p> Release Date : {releaseDate} </p>
         <p> Genre : {genre.join(", ")}</p>
@@ -46,8 +66,13 @@ const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
           rel="noopener noreferrer"
         >
          {watchOn.name}
-        </a></p><br/></div>
+        </a></p>
         </div>
+        <div className="container">
+     <iframe  src={`${trailer}?controls=1&autoplay=1&mute=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="video"></iframe> </div>
+      
+       
+        </div>  </div>
   ))}
     </div>
   );
