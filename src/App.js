294f989948e9b9a89e7e47b099a8e981ca4eef63
Sathish Kumar <84026483+sathishwebdev@users.noreply.github.com>
2021-10-23 11:34:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
       document.getElementById(temp[1]).style.border = "none";
       document.getElementById(temp[1]).style.backgroundColor= "transparent"
       document.getElementById(temp[1]).style.boxShadow="none"
+      document.getElementById(temp[1]).style.padding = "2px"
     document.getElementById(id).style.border = "1px grey solid"
     document.getElementById(id).style.borderRadius="5%"
     document.getElementById(id).style.backgroundColor="gray"
     document.getElementById(id).style.boxShadow="black 1px 2px 15px"
+    document.getElementById(id).style.padding = "6px"
   
   }
    
@@ -49,27 +51,18 @@ const [movie, setMovie] = useState(movies.filter(data=>data.id === '1'))
     
  
        <div className="content "> 
-      <a
-          className="App-link"
-          href={watchOn.link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >  <img src={poster} className="contentImg" alt={name} title={name} /> </a> <p> <a href={`https:youtu.be/${trailer.split("/")[trailer.split("/").length-1]}`} className="App-link"> Watch Trailer  </a></p>
+        <a className="App-link" href={watchOn.link} target="_blank" rel="noopener noreferrer" >  <img src={poster} className="contentImg" alt={name} title={name} /> </a> 
+        <p> <a href={`https:youtu.be/${trailer.split("/")[trailer.split("/").length-1]}`} className="App-link"> Watch Trailer  </a></p>
         <p> category :  {category} </p>
         <p> Release Date : {releaseDate} </p>
         <p> Genre : {genre.join(", ")}</p>
         <p> Description : {summary}</p>
-       <p> Watch On <a
-          className="App-link"
-          href={watchOn.link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         {watchOn.name}
-        </a></p>
+        <p> Watch On <a className="App-link" href={watchOn.link} target="_blank" rel="noopener noreferrer" > {watchOn.name} </a></p>
         </div>
+        {/* video */}
         <div className="container">
-     <iframe  src={`${trailer}?controls=1&autoplay=1&mute=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="video"></iframe> </div>
+     <iframe  src={`${trailer}?controls=1&autoplay=1&mute=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="video"></iframe> 
+     </div>
       
        
         </div>  </div>
