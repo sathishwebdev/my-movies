9fc135a4d4766f205d2ed578a57076507a269466
Sathish Kumar <sathishweb27@gmail.com>
2021-10-27 19:58:06 +0530

    Create notExists.js

---
+++ src/notExists.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useHistory } from 'react-router'
+import * as mui from '@mui/material'
+import * as Icons from '@mui/icons-material'
+
+
+function NotExists() {
+    let history = useHistory()
+    return (
+      <div className="splash">
+        <div>
+          <mui.Button
+          variant="text"
+          color="warning"
+          onClick={()=>history.push('/')}
+          ><Icons.KeyboardArrowLeft />  Go Back To Home</mui.Button>
+        </div>
+        
+        <div> 
+        <img width="100%" alt="404" title="404 - return to home" src='https://media1.giphy.com/media/hS42TuYYnANLFR9IRQ/giphy.gif?cid=6c09b952d7kfjcjmq95xpdxj2t2oczysznih93625vwosnzc&rid=giphy.gif&ct=s' />
+        </div>
+      </div>
+    )
+}
+
+export default NotExists
