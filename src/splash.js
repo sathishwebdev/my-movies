3ea5acb3979836f61ee473cac712798d6afba1ed
Sathish Kumar <sathishweb27@gmail.com>
2021-10-27 10:49:06 +0530

    splash

---
+++ src/splash.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
+function Splash() {
+    return (
+        <div className="splash" >
+            <h1><Link to="/movies" className="link App-link" > <Button
+            variant="text"
+            className="link"
+            >  Movies</Button> </Link> </h1>
+        </div>
+    )
+}
+
+export default Splash
